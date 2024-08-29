b85c38327822689e7d16c6057426cf579cb7b313
OlegDev <olgmrko.dev@gmail.com>
2024-08-29 10:52:08 +0200

    Commit current progress App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import {userSlice} from './store/reducers/UserSlice';
 
 function App() {
   const { count } = useAppSelector(state => state.userReducer);
-  const { increment } = userSlice.actions;
-  const dispatch = useAppDispatch();
+  // const { increment } = userSlice.actions;
+  // const dispatch = useAppDispatch();
 
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button onClick={() => dispatch(increment(10))}>INCREMENT</button>
+      {/* <button onClick={() => dispatch(increment(10))}>INCREMENT</button> */}
     </div>
   );
 }
